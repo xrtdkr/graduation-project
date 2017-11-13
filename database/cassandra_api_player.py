d7670677c2222f7f2bc0bba5d3db39ca44b15f68
XRTDKR <177283052@qq.com>
2017-11-14 01:06:40 +0800

    add git ignore

--- database/cassandra_api_player.py
+++ database/cassandra_api_player.py
@@ -1,13 +1,22 @@
 from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+from cassandra import ConsistencyLevel
 
 cluster = Cluster()
 
 session = cluster.connect("tutorialspoint")
 
 
-query = "INSERT INTO table_1 (column_1, column_2, column_3, column_4) VALUES (1,2,'3',4);"
+query_sql = "INSERT INTO table_1 (column_1, column_2, column_3, column_4) VALUES (1,2,'3',4);"
+
+
+query_deco = SimpleStatement(
+    query_string=query_sql,
+    consistency_level=ConsistencyLevel.ANY
+)
+
 for index in xrange(10):
-    session.execute(query)
+    session.execute(query_deco)
 
 rows = session.execute('SELECT column_1, column_2, column_3, column_4 FROM table_1')
 
